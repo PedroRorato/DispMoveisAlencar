ef29ba64f9898b1466a2a8d305dcdd5f1508d99f
PedroRorato <pedroberorato@gmail.com>
2021-09-01 19:16:27 -0300

    Start

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open App.js to start working!</Text>
       <StatusBar style="auto" />
+      <ActivityIndicator size="large" color="#61dbfb" />
+      <TouchableHighlight>
+        <Text>Hi</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
